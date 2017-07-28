0f9d42db6024bde84fe91b0b6174c6542bc5bff5
zhangbaoxing <zbx@huyi.cn>
2017-07-28 16:09:29 +0800

    update

--- data/tpl_compile/default/EmailTpl/1.html.php
+++
@@ -1,5 +0,0 @@
-<?php
- /* compiled by (FnPHP) at (2017-07-16 18:58:40) */
-?>
-asfdsafds
-asdfdsaf
--- data/tpl_compile/default/footer.html.php
+++
@@ -1,6 +0,0 @@
-<?php
- /* compiled by (FnPHP) at (2017-07-16 18:58:40) */
-?>
-</div>
-</body>
-</html>
